7becdec156e22f46f60821f999056045e20fb898
xMt10 <mturpcu480@gmail.com>
2023-07-01 23:21:14 +0300

    localStorage was added.

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const container = document.querySelector(".container");
-const select = document.querySelector("#movie");
 const amount = document.querySelector(".amount");
 const count = document.querySelector(".count");
+const select = document.querySelector("#movie");
+const seats = document.querySelectorAll(".seat:not(.reserved)");
+
+getFromLocalStorage();
 
 container.addEventListener("click", function (e) {
-  console.log(e);
   if (
     e.target.classList.contains("seat") &&
     !e.target.classList.contains("reserved")
@@ -18,8 +20,46 @@ container.addEventListener("click", function (e) {
 select.addEventListener("change", calculateTotal());
 
 function calculateTotal() {
-  let selectedSeatCount = container.querySelectorAll(".seat.selected").length;
-  let price = select.value;
+  const selectedSeats = container.querySelectorAll(".seat.selected");
+
+  const selectedSeatsArr = [];
+  const seatsArr = [];
+
+  selectedSeats.forEach((seat) => selectedSeatsArr.push(seat));
+
+  seats.forEach((seat) => seatsArr.push(seat));
+
+  let selectedSeatIndexs = selectedSeatsArr.map((seat) =>
+    seatsArr.indexOf(seat)
+  );
+
+  console.log(selectedSeatIndexs);
+
+  let selectedSeatCount = selectedSeats.length;
   count.innerText = selectedSeatCount;
-  amount.innerText = price * selectedSeatCount;
+  amount.innerText = select.value * selectedSeatCount;
+
+  saveToLocalStorage(selectedSeatIndexs);
+}
+
+function getFromLocalStorage() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+
+  if (selectedSeats != null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+  if (selectedMovieIndex != null) {
+    select.selectedIndex = selectedMovieIndex;
+  }
+}
+
+function saveToLocalStorage(indexs) {
+  localStorage.setItem("selectedSeats", JSON.stringify(indexs));
+  localStorage.setItem("selectedMovieIndex", select.selectedIndex);
 }
